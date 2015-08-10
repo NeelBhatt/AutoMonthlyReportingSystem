70bb139e35bd7008142adc21f41fa7007b0bf77f
NeelBhatt <neel.bhatt40@gmail.com>
2015-08-10 11:03:59 +0530

    Update ICommonUtilities.cs

--- Source/Applications/AutomatedReportingSystem.Business/Interfaces/ICommonUtilities.cs
+++ Source/Applications/AutomatedReportingSystem.Business/Interfaces/ICommonUtilities.cs
@@ -1,1 +1,13 @@
 
+using System.Collections.Generic;
+
+namespace AutomatedReportingSystem.Business.Interfaces
+{
+    /// <summary>
+    /// interface for Common Utilities
+    /// </summary>
+    public interface ICommonUtilities
+    {
+        IList<string> Split(string input);
+    }
+}
