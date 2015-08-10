8549f6cc23a3e9ae9436ccdeccf7f1c6c464869a
NeelBhatt <neel.bhatt40@gmail.com>
2015-08-10 11:05:22 +0530

    Update IFileOperations.cs

--- Source/Applications/AutomatedReportingSystem.Business/Interfaces/IFileOperations.cs
+++ Source/Applications/AutomatedReportingSystem.Business/Interfaces/IFileOperations.cs
@@ -1,1 +1,16 @@
 
+using System.Collections.Generic;
+using AutomatedReportingSystem.Business.Models;
+
+namespace AutomatedReportingSystem.Business.Interfaces
+{
+    /// <summary>
+    /// interface for FileOperations
+    /// </summary>
+    public interface IFileOperations
+    {
+        void InitializeProcess(string sourceDirectory);
+
+        List<InvoiceDetails> GetFileData(string sourceDirectory);
+    }
+}
