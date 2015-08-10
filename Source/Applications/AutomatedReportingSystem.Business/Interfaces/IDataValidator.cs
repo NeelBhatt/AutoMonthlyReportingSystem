82239a52ac7d7fd107d3bd7ad795ebd8d47d021e
NeelBhatt <neel.bhatt40@gmail.com>
2015-08-10 11:04:26 +0530

    Update IDataValidator.cs

--- Source/Applications/AutomatedReportingSystem.Business/Interfaces/IDataValidator.cs
+++ Source/Applications/AutomatedReportingSystem.Business/Interfaces/IDataValidator.cs
@@ -1,1 +1,20 @@
 
+using System.Collections.Generic;
+using AutomatedReportingSystem.Business.Models;
+
+namespace AutomatedReportingSystem.Business.Interfaces
+{
+    /// <summary>
+    /// interface for Data Validator
+    /// </summary>
+    public interface IDataValidator
+    {
+        List<string> ValidateCustomerNumber(List<InvoiceDetails> table);
+
+        List<string> ValidateInvoiceNumber(List<InvoiceDetails> table);
+
+        List<string> ValidateInvoiceAmount(List<InvoiceDetails> table);
+
+        List<string> ValidateInvoiceDate(List<InvoiceDetails> table);
+    }
+}
