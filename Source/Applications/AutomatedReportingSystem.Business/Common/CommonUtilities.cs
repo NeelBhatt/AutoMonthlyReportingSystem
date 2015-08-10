2988c77e4d2eb8c122e7c200fe6c1a595e678c63
NeelBhatt <neel.bhatt40@gmail.com>
2015-08-10 11:03:18 +0530

    Update CommonUtilities.cs

--- Source/Applications/AutomatedReportingSystem.Business/Common/CommonUtilities.cs
+++ Source/Applications/AutomatedReportingSystem.Business/Common/CommonUtilities.cs
@@ -1,1 +1,33 @@
 
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using AutomatedReportingSystem.Business.Interfaces;
+
+namespace AutomatedReportingSystem.Business.Common
+{
+    public class CommonUtilities : ICommonUtilities
+    {
+        public IList<string> Split(string input)
+        {
+            return input.Split('\t').ToList();
+        }
+
+        public static string FormatDateToDisplayFromService(object dateTime)
+        {
+            string strDt = Convert.ToString(dateTime);
+            if (strDt.Length == 0) return string.Empty;
+            DateTime dt;
+            DateTime.TryParse(strDt, out dt);
+            if (dt == DateTime.MinValue)
+            {
+                
+            }
+            if (dt != DateTime.MinValue)
+
+                return dt.ToString(ConfigurationManager.AppSettings["DisplayDateFormat"]);
+            return string.Empty;
+        }
+    }
+}
