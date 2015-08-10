50ea8dbe928327d97af37203bf9174ffde9d77bd
NeelBhatt <neel.bhatt40@gmail.com>
2015-08-10 11:04:59 +0530

    Update IDatatableOperations.cs

--- Source/Applications/AutomatedReportingSystem.Business/Interfaces/IDatatableOperations.cs
+++ Source/Applications/AutomatedReportingSystem.Business/Interfaces/IDatatableOperations.cs
@@ -1,1 +1,15 @@
 
+using AutomatedReportingSystem.Business.Models;
+
+namespace AutomatedReportingSystem.Business.Interfaces
+{
+    /// <summary>
+    /// interface for Datatable Operations
+    /// </summary>
+    public interface IDatatableOperations
+    {
+        InvoiceDetails CreateEmptyDataTable(string firstLine);
+
+        InvoiceDetails AddRow(InvoiceDetails dataTable, string line);
+    }
+}
